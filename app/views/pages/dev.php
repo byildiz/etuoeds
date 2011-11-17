47dcccde9afd283c68ccf3268d6337d418718a19
Burak Yildiz <yildizburak@msn.com>
2011-11-17 22:40:59 +0200

    gelistiriciler sayfasi yeni eklendi

---
+++ app/views/pages/dev.php
@@ -0,0 +1,22 @@
+<div id="container">
+  <div id="title">Geliştiriciler</div>
+  <div id="content">
+    <div class="info">
+      <p>Bizler ETÜ OEDS'yi Ortak Eğitim Sistemi'nin belirsizlikleri arasında
+        firmalar hakkında daha kolay ve daha hızlı bilgi sunan ve kullanışlı bir
+        site olması için geliştiriyoruz. Bunun için senin de katkına ihtiyacımız
+        var.</p>
+      <p>Bize bir çok yönden katkıda bulunabilirsin:</p>
+      <ul>
+      	<li>Ortak eğitim yaptığın firmalar hakkında değerlendirme <a href="comment.php?s=select">yazabilirsin</a>,</li>
+        <li>Sorulan sorulara cevaplar <a href="comment.php?select=asked">yazabilirsin</a>,</li>
+        <li>Geliştirici olabilirsin ve siteye yeni özelliklerin eklenmesinde,
+          eksiklerinin giderilmesinde ve tasarımının geliştirilmesinde katkıda
+          bulunabilirsin. Geliştirici olmak için <b><a href="https://github.com/byildiz/etuoeds">github</a></b>
+          sayfamızı ziyaret edebilirsin.</li>
+      </ul>
+      <p>Daha fazla bilgi için bizimle iletişime geçmekten çekinme.</p>
+      <p>E-posta adresimiz: <b>info@etuoeds.com</b></p>
+    </div>
+  </div>
+</div>
