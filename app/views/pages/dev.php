b1f42b573935c106e1a00cea0e4bc063e94e5c12
Burak Yildiz <yildizburak@msn.com>
2011-11-17 23:16:50 +0200

    gelistirici sayfasi duzenlendi

--- app/views/pages/dev.php
+++ app/views/pages/dev.php
@@ -2,14 +2,14 @@
   <div id="title">Sende Katkıda Bulun</div>
   <div id="content">
     <div class="info">
-      <p>Bizler ETÜ OEDS'yi Ortak Eğitim Sistemi'nin belirsizlikleri arasında
+      <p>Biz ETÜ OEDS'yi Ortak Eğitim Sistemi'nin belirsizlikleri arasında
         firmalar hakkında daha kolay ve daha hızlı bilgi sunan ve kullanışlı bir
         site olması için geliştiriyoruz. Bunun için senin de katkına ihtiyacımız
         var.</p>
       <p>Bize bir çok yönden katkıda bulunabilirsin:</p>
       <ul>
-      	<li>Ortak eğitim yaptığın firmalar hakkında değerlendirme <a href="comment.php?s=select">yazabilirsin</a>,</li>
-        <li>Sorulan sorulara cevaplar <a href="comment.php?select=asked">yazabilirsin</a>,</li>
+      	<li>Ortak eğitim yaptığın firmalar hakkında değerlendirme <b><a href="comment.php?s=select">yazabilirsin</a></b>,</li>
+        <li>Sorulan sorulara cevaplar <b><a href="comment.php?select=asked">yazabilirsin</a></b>,</li>
         <li>Geliştirici olabilirsin ve siteye yeni özelliklerin eklenmesinde,
           eksiklerinin giderilmesinde ve tasarımının geliştirilmesinde katkıda
           bulunabilirsin. Geliştirici olmak için <b><a href="https://github.com/byildiz/etuoeds">github</a></b>
